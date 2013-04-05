d8dae3ad024617cc04a4c4715db459f829775d4a
Erik Nomitch <enomitch@gmail.com>
2013-04-04 20:45:46 -0700

    removed xpath calls

--- item.rb
+++ item.rb
@@ -5,17 +5,7 @@ class Item < AttributeInitializable
 
   def add()
     puts "Adding item #{id}..."
-
     @@browser.execute_script("InstacartStore.cart.addItem(#{id}, #{quantity})")
-
-    # Open the detailed view of the Item
-    #@@browser.goto "http://instacart.com/store#items/#{id}"
-
-    ## Click the "Add to Cart" button
-    #@@browser.element(:xpath, "//header[@class='clearfix detail-he=> []]//button[@class='btn btn-large btn-qty inc']").click
-
-    #@@browser.element(:xpath, "//li[@data-item-id='#{id}']//button[@class='btn btn-add-to-cart']").click
-
   end
 
   def self.add_items
